c1a58e1fe3a15b949dd5fa6f026ed5cc3e447e75
wgm22ar <wgm22ar@gmail.com>
2024-09-21 20:50:51 -0300

    carrito arreglos

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 
 const express = require("express"); 
 const productRouter = require("./routes/products.router.js");
-//const cartRouter = require("./routes/carts.router.js");
+const cartRouter = require("./routes/carts.router.js");
 const app = express(); 
 const PUERTO = 8080;
 
@@ -12,7 +12,7 @@ app.use(express.json());
 
 //Rutas
 app.use("/api/products", productRouter);
-//app.use("/api/carts", cartRouter);
+app.use("/api/cart", cartRouter);
 
 
 app.listen(PUERTO, () => {
